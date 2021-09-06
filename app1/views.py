0b1d63b73f6bb96f5b6353052a2a472646f49d90
pritissambare <pritissambare@gmail.com>
2021-09-06 15:41:52 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+import datetime
+
+# Create your views here.
+def fun(request):
+    msg = datetime.datetime.now()
+    return render('base.html',context = {'msg':msg})
